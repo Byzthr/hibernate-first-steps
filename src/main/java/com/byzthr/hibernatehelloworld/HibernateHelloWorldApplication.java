530dc1048cce754778f56d4389c46b5e53b6ee69
Byzthr <byzthr@outlook.com>
2024-01-18 14:51:46 +0100

    Actual Spring setup@

---
+++ src/main/java/com/byzthr/hibernatehelloworld/HibernateHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.byzthr.hibernatehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byzthr/hibernatehelloworld/HibernateHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byzthr.hibernatehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
